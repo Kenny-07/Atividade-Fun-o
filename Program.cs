0880e989414242bc9d1b3f03de7c03e9917f4cb0
DEV <dev@info.local>
2020-11-20 15:29:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade_Fun_o
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
